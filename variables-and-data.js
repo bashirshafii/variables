f1aaa61155140826a00af23862acbcfc5ba8ddd0
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-05 03:21:28 -0500

    operators

--- variables-and-data.js
+++ variables-and-data.js
@@ -9,3 +9,15 @@ let subTotal =  100;
 let tax = subTotal * .07;
 let TotalWithTax = subTotal + tax;
 console.log(TotalWithTax);
+
+
+let add = 2+3;
+let sub = 8-5;
+let multi = 4*5;
+let division = 8/2;
+let mod = 9 % 2;
+console.log(add);
+console.log(sub);
+console.log(multi);
+console.log(division);
+console.log(mod);
