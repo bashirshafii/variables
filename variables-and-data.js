92b3a95922080f89108599f4cef8c68227dcf24e
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-05 02:55:43 -0500

    naming variables

---
+++ variables-and-data.js
@@ -0,0 +1,10 @@
+/*
+variables, data types and typing
+ */
+let  WordData = "Word List";
+console.log(WordData);
+
+
+let subTotal =  100;
+let tax = subTotal * .07;
+let TotalWithTax = subTotal + tax;
